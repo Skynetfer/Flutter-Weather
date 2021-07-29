6cf1f98c987d1a9d6c461918bc48010282be965c
anhbc <anhbcse05256@gmail.com>
2021-07-29 14:19:51 +0700

    Final done

---
+++ lib/models/location.dart
@@ -0,0 +1,12 @@
+class Location {
+  final String city;
+  final String country;
+  final String lattitude;
+  final String longitude;
+
+  Location(
+      {required this.city,
+      required this.country,
+      required this.lattitude,
+      required this.longitude});
+}
--- lib/models/weather.dart
+++ lib/models/weather.dart
@@ -4,13 +4,21 @@ class Weather {
   late final double low;
   late final double high;
   late final String description;
+  late final double pressure;
+  late final double humidity;
+  late final double wind;
+  late final String icon;
 
   Weather(
       {required this.temp,
       required this.feelsLike,
       required this.low,
       required this.high,
-      required this.description});
+      required this.description,
+      required this.pressure,
+      required this.humidity,
+      required this.wind,
+      required this.icon});
 
   factory Weather.fromJson(Map<String, dynamic> json) {
     return Weather(
@@ -19,6 +27,10 @@ class Weather {
       low: json['main']['temp_min'].toDouble(),
       high: json['main']['temp_max'].toDouble(),
       description: json['weather'][0]['description'],
+      pressure: json['main']['pressure'].toDouble(),
+      humidity: json['main']['humidity'].toDouble(),
+      wind: json['wind']['speed'].toDouble(),
+      icon: json['weather'][0]['icon'],
     );
   }
 }
